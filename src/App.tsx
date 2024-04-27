90686cc89c548bf4ef5219161397ce0083995f44
Pasindu dineth <pasindudinethpeiris@gmail.com>
2024-04-27 11:24:25 -0600

    feat: button and card finalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
-
+import { Button } from './stories/Button/Button'
+import { Card } from './stories/Card/Card'
 function App() {
   return (
     <>
       <h1 className="text-quaternary font-bold  text-normal underline font-sans">
         Welcome to Tailwind CSS Installation Tutorial.
       </h1>
+      <Button label='Login' size='xs' color='quaternary'/>
+      <Button label='Login' size='md' color='primary'/>
+      <Button label='Login' size='lg' color='secondary'/>
+      <Card primary/>
+      <Card/>
     </>
   )
 }
--- src/stories/Card/Card.stories.tsx
+++ src/stories/Card/Card.stories.tsx
@@ -20,11 +20,14 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
+    title: 'Card Title',
+    content: 'This is a sample of a Card component. You can pass a string as a property to change this. Please change type or any other property to get a new look and feel or activate any other features such as buttons.',
+
   },
 };
 export const Secondary: Story = {
   args: {
     title: 'Card Title',
-    content: 'This is a sample of a Card component',
+    content: 'This is a sample of a Card component. You can pass a string as a property to change this. Please change type or any other property to get a new look and feel or activate any other features such as buttons.',
   },
 }
